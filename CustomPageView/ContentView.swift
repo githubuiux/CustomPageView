7cd5e8bac33787f9db52f2a262cc6f18089f8d67
githubuiux <uiuxista@gmail.com>
2023-09-11 16:52:46 +0300

    Initial Commit

---
+++ CustomPageView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomPageView
+//
+//  Created by Levent on 11.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomPageView/CustomPageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPageViewApp.swift
+//  CustomPageView
+//
+//  Created by Levent on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomPageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
